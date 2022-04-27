b91eebdaaa4e43586d307423c9d2bd41ab1e09df
Illia Marchenko <ktckto@ukr.net>
2022-04-27 13:09:36 +0300

    clean config.php

---
+++ config.example.php
@@ -0,0 +1,18 @@
+<?php
+$config=[
+    'main'=>[
+        'token'=>''
+    ],
+    'hcaptcha'=>[
+        'public'=>'',
+        'secret'=>''
+    ]
+];
+Class DB_CONFIG {
+    const DB_HOST = '127.0.0.1'; // localhost
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+    const DB_NAME = 'bots';
+    const CHARSET = 'utf8';
+    const DB_PREFIX = '';
+}
